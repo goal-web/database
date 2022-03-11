507e6c3198572a310de031e82e324d16e2bb80a5
桥边红药 <96qbhy@gmail.com>
2022-03-11 11:45:48 +0800

    feat: 优化数据库解析

--- support/parser.go
+++ support/parser.go
@@ -1,26 +1,41 @@
 package support
 
 import (
+	"database/sql"
 	"github.com/goal-web/collection"
 	"github.com/goal-web/contracts"
 	"github.com/jmoiron/sqlx"
+	"strconv"
 )
 
 func ParseRows(rows *sqlx.Rows) (results []contracts.Fields, err error) {
-	if rows.Err() != nil {
-		return nil, rows.Err()
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	columns, colErr := rows.Columns()
+	if colErr != nil {
+		return nil, colErr
+	}
+	colTypes, typeErr := rows.ColumnTypes()
+	if typeErr != nil {
+		return nil, typeErr
 	}
 
+	columnsLen := len(columns)
 	for rows.Next() {
-		var item = make(contracts.Fields)
-		err = rows.MapScan(item)
-		if err != nil {
-			return nil, err
+		var colVar = make([]interface{}, columnsLen)
+		for i := 0; i < columnsLen; i++ {
+			SetColVarType(&colVar, i, colTypes[i].DatabaseTypeName())
 		}
-
-		results = append(results, item)
+		result := make(map[string]interface{})
+		if scanErr := rows.Scan(colVar...); scanErr != nil {
+			panic(scanErr)
+		}
+		for j := 0; j < columnsLen; j++ {
+			SetResultValue(&result, columns[j], colVar[j], colTypes[j].DatabaseTypeName())
+		}
+		results = append(results, result)
 	}
-
 	return
 }
 
@@ -30,4 +45,198 @@ func ParseRowsToCollection(rows *sqlx.Rows) (contracts.Collection, error) {
 		return nil, parseErr
 	}
 	return collection.MustNew(data), nil
+}
+
+func SetColVarType(colVar *[]interface{}, i int, typeName string) {
+	switch typeName {
+	case "INT":
+		var s sql.NullInt64
+		(*colVar)[i] = &s
+	case "TINYINT":
+		var s sql.NullInt64
+		(*colVar)[i] = &s
+	case "MEDIUMINT":
+		var s sql.NullInt64
+		(*colVar)[i] = &s
+	case "SMALLINT":
+		var s sql.NullInt64
+		(*colVar)[i] = &s
+	case "BIGINT":
+		var s sql.NullInt64
+		(*colVar)[i] = &s
+	case "FLOAT":
+		var s sql.NullFloat64
+		(*colVar)[i] = &s
+	case "DOUBLE":
+		var s sql.NullFloat64
+		(*colVar)[i] = &s
+	case "DECIMAL":
+		var s []uint8
+		(*colVar)[i] = &s
+	case "DATE":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "TIME":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "YEAR":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "DATETIME":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "TIMESTAMP":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "VARCHAR":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "MEDIUMTEXT":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "LONGTEXT":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "TINYTEXT":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	case "TEXT":
+		var s sql.NullString
+		(*colVar)[i] = &s
+	default:
+		var s interface{}
+		(*colVar)[i] = &s
+	}
+}
+
+func SetResultValue(result *map[string]interface{}, index string, colVar interface{}, typeName string) {
+	switch typeName {
+	case "INT":
+		temp := *(colVar.(*sql.NullInt64))
+		if temp.Valid {
+			(*result)[index] = temp.Int64
+		} else {
+			(*result)[index] = nil
+		}
+	case "TINYINT":
+		temp := *(colVar.(*sql.NullInt64))
+		if temp.Valid {
+			(*result)[index] = temp.Int64
+		} else {
+			(*result)[index] = nil
+		}
+	case "MEDIUMINT":
+		temp := *(colVar.(*sql.NullInt64))
+		if temp.Valid {
+			(*result)[index] = temp.Int64
+		} else {
+			(*result)[index] = nil
+		}
+	case "SMALLINT":
+		temp := *(colVar.(*sql.NullInt64))
+		if temp.Valid {
+			(*result)[index] = temp.Int64
+		} else {
+			(*result)[index] = nil
+		}
+	case "BIGINT":
+		temp := *(colVar.(*sql.NullInt64))
+		if temp.Valid {
+			(*result)[index] = temp.Int64
+		} else {
+			(*result)[index] = nil
+		}
+	case "FLOAT":
+		temp := *(colVar.(*sql.NullFloat64))
+		if temp.Valid {
+			(*result)[index] = temp.Float64
+		} else {
+			(*result)[index] = nil
+		}
+	case "DOUBLE":
+		temp := *(colVar.(*sql.NullFloat64))
+		if temp.Valid {
+			(*result)[index] = temp.Float64
+		} else {
+			(*result)[index] = nil
+		}
+	case "DECIMAL":
+		if len(*(colVar.(*[]uint8))) < 1 {
+			(*result)[index] = nil
+		} else {
+			(*result)[index], _ = strconv.ParseFloat(string(*(colVar.(*[]uint8))), 64)
+		}
+	case "DATE":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "TIME":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "YEAR":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "DATETIME":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "TIMESTAMP":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "VARCHAR":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "MEDIUMTEXT":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "LONGTEXT":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "TINYTEXT":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	case "TEXT":
+		temp := *(colVar.(*sql.NullString))
+		if temp.Valid {
+			(*result)[index] = temp.String
+		} else {
+			(*result)[index] = nil
+		}
+	default:
+		(*result)[index] = colVar
+	}
 }
